adced90d1d656e3610768c3faab36c224e7e1f29
Alex Z <aleksey.zhd@gmail.com>
2017-05-07 15:29:05 +0600

    Доделал страницу 404

--- page_404.php
+++ page_404.php
@@ -1,13 +1,19 @@
-<link rel="stylesheet" href="css/styles.css">
-
-<?php
-			
-    echo "<div id='page_404'>" .
-         "<h1>Страница 404</h1>" .
-         "<br>" .
-         "<p>По данному коду URL в базе не найден!</p>" .
-         "<br>" .
-         "<a href='index.php'>На главную</a>" .
-         "</div>";
-
-?>
+<!DOCTYPE html> 
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Page 404</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<?php
+			echo "<div id='page_404'>" .
+				 "<h1>Страница 404</h1>" .
+				 "<br>" .
+				 "<p>По данному коду URL в базе не найден!</p>" .
+				 "<br>" .
+				 "<a href='index.php'>На главную</a>" .
+				 "</div>";
+		?>
+	</body>
+</html>
